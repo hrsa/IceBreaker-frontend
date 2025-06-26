6374a0ade6f0db7754647b7153798939da1ccd31
Anton Cherednichenko <anton@anton.eco>
2025-06-26 16:43:21 +0200

    Add new category images: "confessions" and "dilemmas", and update useCardImages hook accordingly

--- hooks/useCardImages.ts
+++ hooks/useCardImages.ts
@@ -1,9 +1,9 @@
 export const useCardImages = () => {
   const categoryImages: Record<string, any> = {
+    generic: require("../assets/images/categories/generic.png"),
     "d689aac9-8f99-4cd8-aa3f-8d2f2b4bfc66": require("../assets/images/categories/authentic_communication.png"),
     "7e3044aa-72fe-49d1-811e-b6ea9c37fbc4": require("../assets/images/categories/deep_questions.png"),
     "b24a0336-211c-4303-a96a-206345488f1e": require("../assets/images/categories/family_questions.png"),
-    generic: require("../assets/images/categories/generic.png"),
     "17b6fc36-0984-43e0-9736-bcadf7395da3": require("../assets/images/categories/questions_and_tasks.png"),
     "f90a29a5-8640-4edc-9192-c875819e2fe8": require("../assets/images/categories/questions_for_couples.png"),
     "e88cb352-24e1-4a42-9087-bf7de7deca6a": require("../assets/images/categories/questions_in_bed.png"),
@@ -11,6 +11,8 @@ export const useCardImages = () => {
     "4fb9e72f-ddf4-4683-b43e-5826cfdcd764": require("../assets/images/categories/talking_to_yourself.png"),
     "965c0dc4-08b2-4a16-b141-ab4f0707bb95": require("../assets/images/categories/truth_or_dare_cosmo.png"),
     "e1dbfaca-e378-462b-a0e0-40ae6cf71e6e": require("../assets/images/categories/truth_or_dare_unhinged.png"),
+    "2e5f2a66-9017-4c7a-8dfc-fea15bcb4ac4": require("../assets/images/categories/confessions.png"),
+    "e17da5af-62d0-4601-960e-441795dfd10d": require("../assets/images/categories/dilemmas.png"),
   };
 
   const getCategoryImage = (categoryId: string) => {
